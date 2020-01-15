d8cbaa1250ce8391733f13c51f65732872f51112
Jozef Filicki <filicki@greenova.cz>
2020-01-15 10:41:09 +0100

    init

---
+++ Templates/ItemTemplates/Wpf.RxUserControlRoutable/RxUserControlViewModel.cs
@@ -0,0 +1,26 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace $rootnamespace$
+{
+    public class $safeitemname$ : ReactiveObject, IRoutableViewModel, IActivatableViewModel, IDisposable
+    {
+		public $safeitemname$(IScreen screen)
+        {
+            HostScreen = screen;
+        }
+		
+		public string UrlPathSegment { get; } = "$safeitemname$";
+
+        public IScreen HostScreen { get; }
+
+        public ViewModelActivator Activator { get; } = new ViewModelActivator();
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
